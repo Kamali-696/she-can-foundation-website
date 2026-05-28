ea499e7b013bb0edfa7c4d46ab23b89b62125577
Kamali-696 <kamalikamuruganandham@gmail.com>
2026-05-28 14:52:27 +0530

    Updated website

--- js/main.js
+++ js/main.js
@@ -138,8 +138,8 @@ document.addEventListener('DOMContentLoaded', () => {
         }
       });
     }, {
-      threshold: 0.15,
-      rootMargin: '0px 0px -40px 0px'
+      threshold: 0.12,
+      rootMargin: '0px 0px -30px 0px'
     });
 
     animatedElements.forEach(el => observer.observe(el));
